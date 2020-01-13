167133cd097ab814e9ab941f43b10b4bb3ed4b86
mattam1921@gmail.com <mattam1921@gmail.com>
2020-01-13 15:03:30 +0100

    added form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,49 +2,64 @@
 
 namespace App\Controller;
 
-use Doctrine\DBAL\Types\TextType;
-use Doctrine\ORM\Query\Expr\From;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\TelType;
-use Symfony\Component\Routing\Annotation\Route;
 //includes de form entity
-use App\Entity\Form;
+use App\Entity\From;
+use Symfony\Component\HttpFoundation\Response;
+
+//types for the form
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="From stage opdracht")
      */
-    public function index()
+    public function index(Request $request)
     {
         $form = $this->createFormBuilder()
-            ->add('Naam')
-            ->add('Email', EmailType::class)
-            ->add('Telefoonnummer', TelType::class)->getForm();
+            ->add('naam', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Naam'
 
+                ]
+            ])
+            ->add('email', EmailType::class , [
+                'attr' => [
+                    'placeholder' => 'Email'
+                ]
+            ])
+            ->add('telefoonnummer', TelType::class , [
+                'attr' => [
+                    'placeholder' => 'Telefoonnummer'
+                ]
+            ])
+            ->add('verstuur', SubmitType::class , [
+                'attr' => [
+                    'class' => 'btn btn-success'
+                    ]
+            ])->getForm();
+        $form->handleRequest($request);
 
-        if (!empty($_POST['Naam'])) {
-            if (!empty($_POST['Email'])) {
-                if (!empty($_POST['Telefoonnummer'])) {
-                    $submit = new Form();
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
 
-                    $submit->setNaam($_POST['Naam']);
-                    $submit->setEmail($_POST['Email']);
-                    $submit->setTelefoonnummer($_POST['Telefoonnummer']);
-
-                    //link manager aan een variable
-                    $em = $this->getDoctrine()->getManager();
-
-                    //maakt een sql query aan
-                    $em->presist($submit);
-
-                    //voert de query uit
-                    $em->flush();
-                }
-            }
-
+            $em->persist($form);
+//
+//            $em->flush();
+            dd($form);
+            return new Response('Succesvol verstuurd');
         }
-
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
